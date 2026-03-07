9dc0fa44de25e954b227b76f876b4084d4924b82
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-07 22:17:12 +0900

    5번 문제 완성?

--- week2/basic/05_recursion.py
+++ week2/basic/05_recursion.py
@@ -34,10 +34,15 @@ def factorial(n):
     """
     # TODO: base case를 작성하세요
     # n이 0이거나 1이면 1을 반환
-    pass
-    
     # TODO: recursive case를 작성하세요
-    pass
+    match n:
+        case 0:
+            return 0
+        case 1:
+            return 1
+        case _:
+            temp = n * factorial(n-1)
+            return temp
 
 def fibonacci(n):
     """
@@ -51,10 +56,13 @@ def fibonacci(n):
     """
     # TODO: base case를 작성하세요
     # n이 0이면 0, n이 1이면 1 반환
-    pass
-    
     # TODO: recursive case를 작성하세요
-    pass
+    match n:
+        case 0:
+            return 0
+        case 1:
+            return 1
+        case _: return fibonacci(n-1) + fibonacci(n-2)
 
 # 테스트 케이스
 if __name__ == "__main__":
