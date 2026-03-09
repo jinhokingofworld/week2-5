ab6ca516583249b397988b4176471e546a374103
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-09 22:47:11 +0900

    리스트를 문자열로 출력할 때는 print(''.join(list))

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,18 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+
+times = int(input())
+
+for i in range(times):
+    temp = input().split() # 3 ABC temp[0]=3 temp[1]="ABC"
+    string = temp[1]
+    stringList = string
+    p = []
+    
+    for j in range(0, len(stringList)):
+        for k in range(int(temp[0])):
+            p.append(stringList[j])
+
+    print(''.join(p))
+
