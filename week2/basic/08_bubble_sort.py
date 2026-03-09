358a66af0b8ecc647fc9dc5c957fa7da44141893
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-09 09:40:09 +0900

    8번 문제 안 푼것 다시 해결

--- week2/basic/08_bubble_sort.py
+++ week2/basic/08_bubble_sort.py
@@ -74,11 +74,16 @@ def bubble_sort_optimized(arr):
         
         # TODO: 내부 반복문과 교환 로직 구현
         # 교환이 발생하면 swapped = True 설정        
-        pass
-        
+        for j in range(0, n - 1 - i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                swapped = True    
 
         # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
+        if swapped == False:
+            break
 
     return arr
 
