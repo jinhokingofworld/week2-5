96e201781952d2aa505a91abd3b79ef982dfa7c5
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-10 21:28:43 +0900

    하노이탑

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,22 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+N = int(input())
+
+def hanoi(n, start, blank, end):
+    if n == 1:
+        print(str(start) +" "+ str(end))
+        return
+    
+    hanoi(n-1, start, end, blank)
+    hanoi(1, start, blank, end)
+    hanoi(n-1, blank, start, end)
+
+
+print(2**N - 1)
+if N <= 20:
+    hanoi(N, 1, 2, 3)
+
+# N개의 탑중에 N-1개를 빈 칸으로 놓음
+# 밑의 한 개를 목표 칸으로 놓음
+# N-1개의 탑을 목표 칸으로 놓음
