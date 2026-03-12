d5c1ebeabcc1966075ab927815ad65a249f9a204
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-12 20:12:36 +0900

    숨겨진 하 문제 찾음

--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -1,2 +1,19 @@
 # 정수론 - 골드바흐의 추측 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/9020
+
+def removeDuplicates(self, nums):
+    #오름차순 정렬된 배열이 인풋으로 들어옴
+    #counter를 설정해놓고
+    #for로 돌아가면서 같으면 삭제 후 append(_)추가. 다르면 새로운 counter.
+
+    counter = nums[0]
+    for i in range(1, len(nums)):
+        if nums[i] == counter:
+            nums.remove(i)
+            nums.append('_')
+        else:
+            counter = nums[i]
+    
+    return nums
+
+removeDuplicates([1,1,2])
--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,16 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+nums = []
+
+for i in range(9):
+    nums.append(int(input()))
+
+max = nums[0]
+
+for j in range(len(nums)-1):
+    if max < nums[j+1]:
+        max = nums[j+1]
+
+print(max)
+print(nums.index(max)+1)
