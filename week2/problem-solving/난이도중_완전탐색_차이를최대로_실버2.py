54693e11ec171ba8c85cdb1562b2c38978860cb6
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-13 15:38:47 +0900

    1번문제 패스

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,36 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+n = int(input())
+nums = list(map(int, input().split()))
+used = [False] * n
+best = 0
+
+def bt(path):
+    global best
+
+    if len(path) == n:
+        sum = 0
+        for i in range(n-1):
+            sum += abs(nums[i] - nums[i+1])
+        best = max(sum, best)
+        return
+    
+    for i in range(n):
+        if used[i]:
+            continue
+
+        used[i] = True
+        path.append(nums[i])
+
+        bt(path)
+
+        path.pop()
+        used[i] = False
+
+bt([])
+print(best)
+
+
+
+
--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -1,19 +1,19 @@
 # 정수론 - 골드바흐의 추측 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/9020
 
-def removeDuplicates(self, nums):
-    #오름차순 정렬된 배열이 인풋으로 들어옴
-    #counter를 설정해놓고
-    #for로 돌아가면서 같으면 삭제 후 append(_)추가. 다르면 새로운 counter.
+# 2보다 큰 짝수 n이 주어졌을 때, n의 골드바흐 파티션을 출력하는 프로그램을 구하라
 
-    counter = nums[0]
-    for i in range(1, len(nums)):
-        if nums[i] == counter:
-            nums.remove(i)
-            nums.append('_')
-        else:
-            counter = nums[i]
+# 입력 받은 숫자를 반으로 나눔 
+# 하나는 1을 더하고, 하나는 1을 빼는 것을 반복해서 두 수 모두 소수가 되면 프린트
+
+# 6 = 3+3 / 4 = 2+2
+
+def sosu():
+    result = str()
     
-    return nums
+    print(result)
 
-removeDuplicates([1,1,2])
+
+n = int(input())
+for i in range(n):
+    sosu()
