2b0520133bb6a6e2dd67078e3e08dd387583a192
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-15 17:18:22 +0900

    백트래킹이 조금 보이기 시작

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -9,28 +9,30 @@ best = 0
 def bt(path):
     global best
 
+    #모두 선택되었을 때 최댓값 검색 하고, 반환
     if len(path) == n:
         sum = 0
         for i in range(n-1):
-            sum += abs(nums[i] - nums[i+1])
+            sum += abs(path[i] - path[i+1])
         best = max(sum, best)
         return
     
+    #순열 찾기
+    #처음부터 끝까지 안 used가 False인 애들을 찾음
     for i in range(n):
         if used[i]:
             continue
 
+        #선택
         used[i] = True
         path.append(nums[i])
 
+        #탐색
         bt(path)
 
+        #취소
         path.pop()
         used[i] = False
 
 bt([])
 print(best)
-
-
-
-
