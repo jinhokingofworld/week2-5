7bdd451db1b0bdfef13ca6f08636845077f5a25b
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-17 00:10:05 +0900

    두 용액 실패

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,36 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+n = int(input())
+sets = list(map(int, input().split()))
+sets.sort()
+
+result = []
+recorde = float('inf')
+
+def find(arr, index):
+    global result
+    global recorde
+
+    target = arr[index]
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] < -target:
+            left = mid + 1
+        else:
+            right = mid -1 
+    # 종료 상태는 항상 right < left
+    
+    for candidate in [left, right]:
+        if 0 <= candidate < len(arr) and candidate != index:
+            current_recorde = abs(arr[candidate] + target)
+            if current_recorde < recorde:
+                result = [arr[candidate], target]
+
+for i in range(n):
+    find(sets, i)
+result.sort()
+print(" ".join(map(str, result)))
