b159ca0c87a36e6effc00ac88b599106397dbf15
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-21 10:34:15 +0900

    약간 이상한 문제

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,9 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+t = int(input())
+
+for _ in range(t):
+    N, M = map(int, input().split())
+    for _ in range(M):
+        input()
+    print(N-1)
