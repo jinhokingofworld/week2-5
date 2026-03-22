7d483f8f15a153c80adc2bb10c4e9c1c4e5c8a68
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-22 14:23:55 +0900

    리커젼 오류가 났는데, 그 오류를 유발하는 테스트케이스를 작성해볼 것. 점프왕쩰리 해결.

--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,29 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+N = int(input())
+board = [[0] * N for _ in range(N)]
+
+for i in range(N):
+    temp2 = list(map(int, input().split()))
+    for j in range(N):
+        board[i][j] = temp2[j]
+
+def R(x, y):
+    if x == N-1 and y == N-1:
+        return True
+    
+    val = board[x][y]
+    if val != 0:
+        if x + val < N:
+            if R(x + val, y):
+                return True
+        if y + val < N:
+            if R(x, y + val):
+                return True
+
+    return False
+
+if R(0, 0):
+    print('HaruHaru')
+else:
+    print('Hing')
