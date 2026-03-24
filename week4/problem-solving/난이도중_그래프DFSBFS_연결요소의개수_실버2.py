dbf7210a4a36a08ad99730563a8cc42d3134b389
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-24 16:48:03 +0900

    노드를 돌면서 안 본 애들을 상대로 DFS

--- week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
+++ week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
@@ -4,41 +4,30 @@
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
 
-for i in range(M):
+for _ in range(M):
     s, e = map(int, input().split())
     graph[s].append(e)
     graph[e].append(s)
 
 seen = set()
-yet = set()
-for i in range(1, N+1):
-    yet.add(i)
 
 def DFS(start):
-    global seen
     stack = [start]
+    seen.add(start)
 
     while stack:
         current = stack.pop()
-        seen.add(current)
 
         for i in graph[current]:
             if i not in seen:
                 stack.append(i)
-
-#yet에서 하나 pop해서 DFS돌리고, 
-#len(yet - seen) == 0인지 확인
-#yet - seen에서 하나 pop해서 DFS돌리고
-#yet - seen == 0인지 확인
+                seen.add(i)
 
 count = 0
-while True:
-    diff = yet - seen
-    if len(diff) == 0:
-        break
 
-    now = diff.pop()
-    DFS(now)
-    count += 1
+for i in range(1, N+1):
+    if i not in seen:
+        DFS(i)
+        count += 1
 
 print(count)
