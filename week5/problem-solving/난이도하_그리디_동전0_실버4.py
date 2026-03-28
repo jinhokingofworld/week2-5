36c3757c4ee5813f291ed5790311de71e15c0660
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-28 14:42:48 +0900

    동전

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,29 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+import sys
+# from collections import deque
+input = sys.stdin.readline
+
+n, money = map(int, input().split())
+coins = [ int(input()) for _ in range(n)]
+# print(coins)
+result = 0
+
+for c in range(len(coins)):
+    c = coins.pop()
+    current = 0
+    if money < c:
+        continue
+
+    if money >= c:
+        temp = money // c
+        money = money % c
+        result += temp
+        # print(c)
+    # result += current
+
+    if money == 0:
+        break
+
+print(result)
