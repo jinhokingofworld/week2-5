ccf3c44b326cd2793c4fa2dd5526d95c01d8d648
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-28 15:32:06 +0900

    하

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,22 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+from collections import deque
+N = int(input())
+
+def tiles(n):
+
+    if n == 0 or n==1:
+        return 1
+
+    dp = deque([0] * 2)
+    dp[0] = 1
+    dp[1] = 1
+    # print(dp)
+    for _ in range(2, n+1):
+        #중간 값이 너무 커져서 계속 나머지를 나누어주어야 함
+        temp = (dp[0] + dp[1]) % 15746
+        dp.append(temp)
+        dp.popleft()
+    return dp[-1]
+
+print(tiles(N) % 15746)
