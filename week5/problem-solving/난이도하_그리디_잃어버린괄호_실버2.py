cea9d25246ebac685caa6bf9d205145e8be5fbcb
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-28 15:55:29 +0900

    왜 그리디인지 모르겠음

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,16 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+temp1 = input().split("-")
+for j in range(len(temp1)):
+    templist = map(int, temp1[j].split("+"))
+    tempsum = 0
+    for i in templist:
+        tempsum += i
+    temp1[j] = tempsum
+
+result = temp1[0]
+for i in range(1, len(temp1)):
+    result -= temp1[i]
+print(result)
+    
