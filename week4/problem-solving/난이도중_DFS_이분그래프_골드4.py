e0cec71c30044878ca9ddc272ec08c046c382af2
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-29 22:13:43 +0900

    문제 풀다가 포기한 목록 나중에 다시도전

--- week4/problem-solving/난이도중_DFS_이분그래프_골드4.py
+++ week4/problem-solving/난이도중_DFS_이분그래프_골드4.py
@@ -1,2 +1,36 @@
 # DFS - 이분 그래프 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/1707
+from collections import deque
+
+N = int(input())
+
+#인접리스트, 시작점으로 인접리스트 판별
+def BFS(graph, start):
+    #True면 Ateam, False면 Bteam
+    Ateam = set({})
+    Bteam = set({})
+    queue = deque([start])
+    Ateam.add(start)
+
+    while queue:
+        current = queue.popleft()
+
+        #A팀이면 인접리스트가 다 B여야 함
+        if current in Ateam:
+            for i in graph[current]:
+                if i 
+        #B팀이면 인접리스트가 다 A여야 함
+        if current in Bteam:
+
+    
+    
+
+
+for _ in range(N):
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V+1)]
+    for i in range(E):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    BFS(graph, 1)
--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,46 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
+from collections import deque
+import sys
+
+#큐에 봐야할 숫자들을 넣는다.
+q = deque([])
+lines = sys.tedin.readlines()
+for line in lines:
+    q.append(int(line))
+
+#트리를 만들 노드를 생성
+class Node():
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.val = data
+
+s = deque([])
+before = None
+#스택에 숫자를 확인하면서 넣어 트리를 만든다.
+
+#루트 노드 생성
+current = q.popleft()
+s.append(current)
+before = Node(current)
+
+#트리 연결
+while len(q) > 0:
+    current = q.popleft()
+    if s[-1] < current:
+        while s[-1] < current:
+            s.pop()
+    s.append(current)
+    #이전에 나온 값보다 작으면 left에 잇고, 스택 팝하고 크다면 부모노드랑 비교
+    if before.val < current:
+
+#완성된 트리를 DFS한다.
+
+#트래킹 목록
+#현재숫자
+#이전숫자
+#다음숫자
+
+#트리를 어떻게 만들지?
+#트리를
