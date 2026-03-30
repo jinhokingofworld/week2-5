55c232a6a83e097881882de050d2d54a7179583b
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-30 15:03:34 +0900

    DP 진짜 말 안된다

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -39,6 +39,7 @@
     
 # print(dp[-1][1])
 
+#물건 개수 N, 최대 무게 K
 N, K = map(int, input().split())
 items = []
 dp = []
@@ -46,3 +47,26 @@ for i in range(N):
     A, B = map(int, input().split())
     items.append((A, B))
 
+#무게순으로 정렬
+items.sort(key = lambda x: (-x[0], x[1]))
+
+dt = [0] * (K+1)
+
+#무게당 최대 효율을 내는 것
+#인덱스가 해당 무게
+#dt[i] = dt[]
+
+# for i in range(items[0][0], K+1):
+#     for t in items:
+#         #무게가 맞다면,
+#         #가치가 더 큰걸 저장
+#         if i - t[0] >= 0:
+#             dt[i] = max( dt[i], dt[i-t[0]]+ t[1])
+
+for t in items:
+    for i in range(K, 0, -1):
+        if i - t[0] >= 0:
+            dt[i] = max(dt[i], dt[i-t[0]] + t[1])
+
+# print(dt)
+print(dt[K])
