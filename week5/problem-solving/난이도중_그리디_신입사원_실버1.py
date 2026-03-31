af0f650061d29b70da9981e8b24fbc46813c297c
jinhokingofworld <111578154+jinhokingofworld@users.noreply.github.com>
2026-03-31 16:04:17 +0900

    그리디는 정렬하는 것부터 생각

--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,31 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+
+# 1차 서류시험, 2차 면접시험
+#적어도 하나가 다른 지원자보다 떨어지지 않는 사람만 선발
+
+#서류점수나 면접 점수가 최댓값을 가져야 함
+import sys
+input = sys.stdin.readline
+
+testcase = int(input())
+
+for _ in range(testcase):
+    N = int(input())
+    tempC = []
+    for __ in range(N):
+        #A는 서류점수, B는 면접점수
+        A, B = map(int, input().split())
+        tempC.append((A, B))
+    
+    tempC.sort(key= lambda x: x[0])
+
+    bestB = float('inf')
+    count = 0
+
+    for i in tempC:
+        if i[1] < bestB:
+            bestB = i[1]
+            count +=1
+
+    print(count)
